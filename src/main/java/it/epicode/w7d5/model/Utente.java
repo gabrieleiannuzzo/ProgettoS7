d478856236bf940006c986720a779b06b5df886d
gabrieleiannuzzo <gabrieleiannuzzo10@gmail.com>
2024-02-25 14:16:40 +0100

    Aggiunte repositories e sistemato Utente

--- src/main/java/it/epicode/w7d5/model/Utente.java
+++ src/main/java/it/epicode/w7d5/model/Utente.java
@@ -2,11 +2,17 @@ package it.epicode.w7d5.model;
 
 import jakarta.persistence.*;
 import lombok.Data;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
 
 @Data
 @Entity
 @Table(name = "utenti")
-public class Utente {
+public class Utente implements UserDetails {
     @Id
     @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "sequenza_utenti")
     @SequenceGenerator(name = "sequenza_utenti", initialValue = 1, allocationSize = 1)
@@ -18,4 +24,29 @@ public class Utente {
     private String password;
     @Enumerated(EnumType.STRING)
     private Role role;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority(role.name()));
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return UserDetails.super.isAccountNonExpired();
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return UserDetails.super.isAccountNonLocked();
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return UserDetails.super.isCredentialsNonExpired();
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return UserDetails.super.isEnabled();
+    }
 }
---
+++ src/main/java/it/epicode/w7d5/repository/UtenteRepository.java
@@ -0,0 +1,10 @@
+package it.epicode.w7d5.repository;
+
+import it.epicode.w7d5.model.Utente;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UtenteRepository extends JpaRepository<Utente, Integer>, PagingAndSortingRepository<Utente, Integer> {
+}
